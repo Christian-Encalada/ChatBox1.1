2d4f943dd0eba51b24932eff81bc95e50d8b956f
Christian-Encalada <cris_7715@hotmail.com>
2024-01-15 21:42:14 -0500

    intento de notificaciones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,48 @@
+/* global firebase */
+
 import './App.css';
 import { io } from 'socket.io-client';
 import { useState, useEffect } from 'react';
 import { LiMensaje, UlMensajes } from './ui-components';
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwBEW-vmdytwra50NwyHaQhYCqg4s_yV4",
+  authDomain: "chatbox-98fbb.firebaseapp.com",
+  projectId: "chatbox-98fbb",
+  storageBucket: "chatbox-98fbb.appspot.com",
+  messagingSenderId: "314247152483",
+  appId: "1:314247152483:web:ad1e17c335f01bb6da4956"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+
+// Solicitar permiso de notificación al cargar la aplicación
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
+    console.log('Permiso de notificación concedido');
+    return getToken(messaging, { vapidKey: '3gjp1GuVkYSZPPsz04fzhOjIoE0PYUYccu3zRlxmMMQ' });
+  } else {
+    throw new Error('Permiso de notificación denegado');
+  }
+}).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    console.log('Token FCM:', currentToken);
+  } else {
+    console.log('No registration token available. Request permission to generate one.');
+  }
+}).catch((err) => {
+  console.error('An error occurred while retrieving token. ', err);
+});
+
+// Manejar las notificaciones entrantes
+onMessage(messaging, (payload) => {
+  console.log('Notificación recibida:', payload);
+  // Puedes agregar lógica para mostrar la notificación al usuario aquí
+});
 
 const socket = io('http://localhost:3000');
 
@@ -27,10 +68,25 @@ function App() {
 
   const enviarMensaje = () => {
     socket.emit('enviar_mensaje', {
-      usuario: nombreUsuario, // Utiliza el nombre de usuario en lugar de socket.id
-      mensaje: nuevoMensaje
+      usuario: nombreUsuario,
+      mensaje: nuevoMensaje,
     });
-  }
+  
+    // Enviar notificación usando FCM
+    messaging.getToken().then((token) => {
+      const message = {
+        notification: {
+          title: 'Nuevo Mensaje',
+          body: `${nombreUsuario}: ${nuevoMensaje}`,
+        },
+        token: token,
+      };
+  
+      return firebase.messaging().send(message);
+    }).catch((error) => {
+      console.error('Error al enviar la notificación:', error);
+    });
+  };
 
   const unirseAlChat = () => {
     // Cuando el usuario se une al chat, envía su nombre al servidor
